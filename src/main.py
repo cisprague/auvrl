937323d6457ae57cf5d9e564a981ebadb2e7f0a9
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2018-03-19 16:09:07 +0100

    Forgot to call get_proximity to draw the lines on the visualizer, removed normalization flag, normalize the distances yourself

--- src/main.py
+++ src/main.py
@@ -28,6 +28,8 @@ if __name__=='__main__':
     while running:
         running = cont.update(C.TIME_STEP)
         auv.update(C.TIME_STEP)
+        # if this is never called, the visualizer wont show the lines
+        auv.get_proximity()
         world.update(C.TIME_STEP)
         if viz is not None:
             viz.update(points = auv.last_casted_points, points_connection = auv.get_position())
